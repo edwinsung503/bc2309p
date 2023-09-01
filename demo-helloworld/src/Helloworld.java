c34ee7fe5adc1a98bbc822699d54281b0cdfc530
Edwin Sung <edwardwong503@gmail.com>
2023-09-01 16:37:29 +0800

    update test laptop

---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,10 @@
+//not yet add <<code install>> from cntl shift p
+public class Helloworld {
+  public static void main(String [] args) {
+    //Program starts..
+    //Tool: System.out.println
+    System.out.println("Hello World !!!你好! Good Bye");
+    System.out.println("Good Bye Viencent");
+  }
+}
+//this is the end of file
