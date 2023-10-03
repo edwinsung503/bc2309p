72db1d53bf8cbe81bc582f750faa7518c33f2916
edwin sung <edwardwong503@gmail.com>
2023-10-03 10:49:09 +0800

    update on 03Oct

---
+++ demo-class/src/LeetCodeExe.java
@@ -0,0 +1,17 @@
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+
+public class LeetCodeExe {
+  public static void main(String[] args) {
+    int [] nums = new int [] {13,25,83,77};
+    String result="";
+    
+    for (int i=0; i < nums.length;i++) {
+      result += nums[i]/10;
+      result += nums[i] % 10;
+    
+
+
+
+  }
+}
